452251a0a98401cdc01c8cc1b9054e9ddf0ef090
Joseph Sesti <sestijoey63@gmail.com>
2019-08-21 20:30:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(collection)
     name.split(" ").first
     puts "#{name.upcase}."
   end
-  return collection
+  collection
 end
 
 collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
